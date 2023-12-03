678137758d480c7d0f65de382001dbddb537d98a
AsmiMudras6 <asmimudras6@gmail.com>
2023-12-03 12:26:28 +0530

    fixes

--- hhc/frontend/urls.py
+++ hhc/frontend/urls.py
@@ -21,5 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",views.index, name="Home"),
-    path("about/",views.about,name="AboutUs")
+    path("about/",views.about,name="AboutUs"),
+    path("user/",views.user,name="user")
 ]
